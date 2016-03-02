1955975a5946a30a64f156ad8b556e88f0ef879c
Simon Dell <simon@expressionworks.co.uk>
2016-03-02 14:32:22 +0000

    Going through video three or four, where Dan shows how to send actions to the store for the first time.

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 Dan Abramov disrupted the Flux/React community with his Redux project. It was such a powerful mutation of the work Facebook had done that they hired him!
 
 Here's Dan's canonical [Getting Started With Redux](https://egghead.io/lessons/javascript-redux-the-single-immutable-state-tree?series=getting-started-with-redux) tutorial, and my practice project.
+
+## Resources
+
+* [Redux](https://github.com/reactjs/redux)
+* [A tutorial on Mocha & Chai](https://semaphoreci.com/community/tutorials/getting-started-with-node-js-and-mocha)
+* [setting up Babel](https://babeljs.io/docs/usage/cli/)
--- test/redux_tests.js
+++ test/redux_tests.js
@@ -3,6 +3,7 @@ var expect = require( 'chai' ).expect;
 
 // libraries
 var redux = require( 'redux' );
+var tutorial = require( '../build/tutorial' );
 
 describe( 'When testing redux', function () {
 
@@ -10,4 +11,35 @@ describe( 'When testing redux', function () {
         expect( !!redux ).to.be.true;
     })
 
+    it( 'should import the tutorial code', function (){
+			expect( tutorial ).to.exist;
+    });
+
+    describe( 'and the tutorial', function (){
+	    it( 'should expose the store', function (){
+				expect( tutorial.store ).to.exist;
+	    });
+    });
+
+});
+
+
+describe( 'When instanciating a store', function () {
+	it( 'should maintain a state', function () {
+		expect( tutorial.store.getState() ).to.be.defined;
+	});
+
+	it( 'should have an initial state of 0', function () {
+		expect( tutorial.store.getState() ).to.equal( 0 );
+	});
+});
+
+describe( 'When sending actions', function (){
+	describe( 'and the action has type="INCREMENT"', function (){
+		tutorial.store.dispatch({ type: 'INCREMENT' });
+
+		it( 'should update the state of the store (to 1)', function (){
+			expect( tutorial.store.getState() ).to.equal( 1 );
+		});
+	});
 });
